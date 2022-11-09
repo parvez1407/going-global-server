18872936d77af4b05a361331bf4356eb3305ffe0
parvez1407 <info.nahidwd@gmail.com>
2022-11-09 14:09:16 +0600

    create database user and connect server & db and declare a async function

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
 const app = express();
+const port = process.env.PORT || 5000;
 
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ktnfrsc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
+
 
 
 app.get('/', (req, res) => {
